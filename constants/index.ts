687bc7d0ccf6d3be293136aa82d47a57020dcd9a
Tushar <tusharprajapati416@gamil.com>
2024-08-31 23:16:04 +0530

    Your comment

---
+++ constants/index.ts
@@ -0,0 +1,172 @@
+import { RxHome, RxPerson, RxDashboard,  } from "react-icons/rx";
+import { SiMinutemailer } from "react-icons/si";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+ 
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    name: "My Sql",
+    Image: "/mysql.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    name: "Java",
+    Image: "/java.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    name: "Github",
+    Image: "/github.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  
+  {
+    
+    name: "Linkedin",
+    src: "/linkedin.svg",
+    url: "https://www.linkedin.com/in/tushar-prajapati-98260a223/"
+  },
+  {
+    
+    name: "Leetcode",
+    src: "/leetcode.svg",
+    url: "https://leetcode.com/u/tusharprajapati416/"
+  },
+  {
+    name: "GitHub",
+    src: "/github.svg",
+    url: "https://github.com/tusharpraja"
+  },
+  {
+    name: "Instagram",
+    src: "/instagram.svg",
+    url: "https://www.instagram.com/trozen.tushar_05?igsh=NTM5N2Jmemt0ODhm"
+  },
+  {
+    name: "Discord",
+    src: "/discord.svg",
+    url: "https://discord.com/channels/@me"
+  },
+  
+  
+];
+export const Projects = [
+  {
+    title: "E-Clinic",
+    text: "Explore a modern HealthCare Hub website that integrates a ChatGPT-powered chatbot. 🔗LINK: https://e-clinic-gcet.vercel.app/",
+    src: "/EC.png",
+    link: "https://e-clinic-gcet.vercel.app/",
+  },
+  {
+    title: "Multi Player Game",
+    text: " Innovative 3D Multiplayer Shooter Experience with React Three Fiber and Playroom.  🔗LINK: https://tushar-multiplayer.netlify.app/",
+    src: "/MPG.png",
+    link: "https://tushar-multiplayer.netlify.app/",
+  },
+  {
+    title: "Sorting Visualizer",
+    text: "Sorting Visualizer is a web app for visualizing a bunch of different sorting algorithms. 🔗LINK:https://github.com/tusharpraja/Sorting-Visualizer",
+    src: "/SV.png",
+    link: "https://github.com/tusharpraja/Sorting-Visualizer",
+  },
+  {
+    title: "Portfolio",
+    text: "Personal Portfolio",
+    src: "/PW.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: SiMinutemailer,
+    link: "/contact-me",
+  },
+];
